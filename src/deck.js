2c1dafd8236e1af58ad6c95b02f8531384e11d18
Jessica Moore <jessica.cmoore1@gmail.com>
2019-06-25 15:41:58 -0400

    Complete

--- src/deck.js
+++ src/deck.js
@@ -2,10 +2,40 @@ class Deck {
   // We've defined the constructor method, although it's not currently doing anything.
   // This is the perfect place to set up the initial state of any new Deck instances.
   constructor() {
+    this.cards = [];
 
+    for (let i = 1; i <= 52; i++) {
+      this.cards.push(i);
+    }
+
+    
   }
-
+  
   // Any additional instance methods required by the test suite can be defined below.
+    shuffle() {
+      this.cards.sort(() => Math.random() - 0.5);
+      return this.cards;
+    }
+
+    draw (num) {
+      if (this.cards.length === 0) {
+        return -1;
+      } else {   
+        if (num === undefined) {
+          let card = this.cards[this.cards.length-1];
+          this.cards.pop();
+          return card;
+        } else if (typeof num !== 'number') {
+          return;
+        } else {
+          let drawedCards = []
+          for (let i=0; i<num; i++) {
+            drawedCards.push(this.cards.pop());
+          }
+          return drawedCards;
+        }
+      }
+    }
 }
 
 module.exports = Deck;
